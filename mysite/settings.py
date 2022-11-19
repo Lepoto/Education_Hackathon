27bb03ea981851f0488b5172b9d36ec5ad02a51e
Alfred Masekwameng <lepotoalfred18@gmail.com>
2022-11-19 06:32:46 +0200

    quiz

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'quiz.apps.QuizConfig',
+    'sebenza.apps.SebenzaConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
